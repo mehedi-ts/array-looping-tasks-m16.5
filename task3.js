8107b1b0aec746eca42d50d9435513ac0934e6ff
mehedi hasan <mehedihasan.j135@gmail.com>
2026-02-05 18:39:51 +0600

    task3 complete

---
+++ task3.js
@@ -0,0 +1,12 @@
+// Task 3
+// Write a JavaScript code to get the even numbers from an array using any looping technique.
+
+// Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+for (let num of numbers) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+}
